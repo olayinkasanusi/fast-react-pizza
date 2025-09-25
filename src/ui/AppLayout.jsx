60ed2ab6a4feb9a2a7680a635fb388e18619dcc1
Sanusi Olayinka <sanusiolayinka7@gmail.com>
2025-09-25 13:34:26 +0100

    //

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,9 +9,7 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {/* {isLoading && <Loader />}
-       */}
-      <Loader />
+      {isLoading && <Loader />}
       <Header />
 
       <div className="overflow-scroll">
